305f76b0d7023228a7206126b286b91687f7c78e
Simone Tesini <simozilla006@gmail.com>
2023-10-24 23:49:44 +0200

    Initial Commit

---
+++ kebhub/kebhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  kebhubApp.swift
+//  kebhub
+//
+//  Created by Simone Tesini on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct kebhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
