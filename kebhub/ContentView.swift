254c8ee87d6126b109b1c9fa64035776b6f646f4
Simone Tesini <simozilla006@gmail.com>
2023-10-25 18:07:38 +0200

    feat: add likes and dislikes

--- kebhub/ContentView.swift
+++ kebhub/ContentView.swift
@@ -10,23 +10,57 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var kebabbaros: [Kebabbaro]
+    @Query(sort: \Kebabbaro.points, order: .reverse) var kebabbaros: [Kebabbaro]
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(kebabbaros) { k in
-                    Text(k.name).font(.headline)
+                    HStack {
+                        Image(k.imageName).resizable().frame(width: 50, height: 50, alignment: .center).clipShape(.rect(cornerRadius: 5))
+                        
+                        VStack {
+                            HStack {
+                                Text(k.name).font(.headline).frame(width:150, height: 15, alignment: .leading)
+                                Spacer()
+                            }
+                            HStack {
+                                Text(k.address).font(.subheadline).truncationMode(.tail).frame(width: 150, height:10, alignment: .leading)
+                                Spacer()
+                            }
+                        }
+                        
+                        Spacer()
+                        
+                        Text("\(k.points)").foregroundStyle(k.points > 0 ? .green : .red)
+                        
+                        Button() {
+                            k.points += 1
+                        } label: { Image(systemName: "hand.thumbsup") }.buttonStyle(.borderless)
+                        
+                        Button() {
+                            k.points -= 1
+                        } label: { Image(systemName: "hand.thumbsdown") }.buttonStyle(.borderless)
+                    }
                 }
                 .onDelete(perform:deleteKebabbaros)
             }
         .navigationTitle("Kebhub")
         .toolbar {
             Button("Add sample") {
-                modelContext.insert(Kebabbaro(name: "matteotti kebab", imageName: "logo", address: "piazza matteotti", positive: 100000, negative: 0))
+                modelContext.insert(Kebabbaro(name: "Matteotti Turkish", imageName: "matteottikebab", address: "Piazza Matteotti, Bolzano BZ", points: 0))
                 }
             }
         }
+        .onAppear(perform: {
+            guard kebabbaros.isEmpty else { return }
+            for k in Kebabbaro.sampleData {
+                modelContext.insert(k)
+            }
+        })
     }
+    
+    func noop () {}
     
     func deleteKebabbaros(_ indexSet: IndexSet) {
         for index in indexSet {
--- kebhub/Kebabbaro.swift
+++ kebhub/Kebabbaro.swift
@@ -14,14 +14,35 @@ class Kebabbaro {
     var name: String
     var imageName: String
     var address: String
-    var positive: Int
-    var negative: Int
+    var points: Int
     
-    init(name: String, imageName: String, address: String, positive: Int, negative: Int) {
+    init(name: String, imageName: String, address: String, points: Int) {
         self.name = name
         self.imageName = imageName
         self.address = address
-        self.positive = positive
-        self.negative = negative
+        self.points = points
     }
+}
+
+extension Kebabbaro {
+    static var sampleData: [Kebabbaro] = [
+        Kebabbaro(
+            name: "Matteotti Turkish",
+            imageName: "matteottikebab",
+            address: "Piazza Giacomo Matteotti, Bolzano, Italia",
+            points: 100
+        ),
+        Kebabbaro(
+            name: "Skampini",
+            imageName: "skampini",
+            address: "Piazza Domenicani 41, Bolzano, Italia",
+            points: 10
+        ),
+        Kebabbaro(
+            name: "New Kurdistan",
+            imageName: "kurdistan",
+            address: "Corso Italia, 5, Bolzano, Italia",
+            points: 0
+        ),
+    ]
 }
